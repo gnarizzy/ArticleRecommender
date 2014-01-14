28c3b13d54e93e2fc5795c97e18c04c41b2ff6f0
Gautam <millisay2013@gmail.com>
2014-01-14 04:45:11 -0500

    Initial commit

---
+++ recommender/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from recommender.models import Article
+
+admin.site.register(Article)
---
+++ recommender/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Article(models.Model):
+    pub_date = models.DateTimeField()
+    title = models.CharField(max_length=500)
+    CATEGORY_CHOICES = (
+        ('economy','Economy'),
+        ('stem','STEM'),
+        ('middle east','Middle East'),
+        ('pop culture','Pop Culture'),
+        ('elections','Elections'),
+        ('domestic political issues','Domestic Political Issues'),
+        ('international political issues','International Political Issues'),
+        ('georgia','Georgia/UGA'),
+        ('totally international','Totes Interational'),
+
+    )
+    category=models.CharField(max_length=200,choices=CATEGORY_CHOICES)
+    url=models.URLField()
+    content=models.TextField()
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.title
