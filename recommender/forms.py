d53f70561a33285bbbba66c5da4aad689340c051
Gautam <gautam@uga.edu>
2014-03-04 15:28:30 -0500

    Create django form and basic index template

---
+++ recommender/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from recommender.models import Article
+
+class ArticleForm(forms.Form):
+    title = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),max_length=200, help_text="Title")
+    content = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}), help_text="Content")
--- recommender/views.py
+++ recommender/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from recommender.forms import ArticleForm
 
 def index(request):
-    return render(request, 'recommender/index.html')
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+    else:
+        form = ArticleForm()
+    return render(request, 'recommender/index.html',{'form':form,})
