9aa1370ed2e521b6d3b626d7672f77a857ceab7b
Gautam <gautam@uga.edu>
2014-03-07 13:52:37 -0500

    Recommendations now appear after submission

--- recommender/views.py
+++ recommender/views.py
@@ -4,16 +4,21 @@ from recommender.models import Article
 from recommender.forms import ArticleForm
 from recommender.article_classifier import naive_bayes
 
+c1=naive_bayes()
+article_list=Article.objects.all()
+for article in article_list:
+    c1.train(article.content,article.category)  # Should eventually be moved to database
+
 def index(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST)
-        #if form.is_valid(): Add form validation
-        c1=naive_bayes()
-        article_list=Article.objects.all()
-        for article in article_list:
-            c1.train(article.content,article.category) #Should eventually be moved to database
-
-
+        if form.is_valid(): #Add form validation
+            form_title = form.cleaned_data['title']
+            cat = c1.classify(form.cleaned_data['content'])
+            recommended_list = Article.objects.filter(category=cat).order_by('-pub_date')[:3]
+            form = ArticleForm()
+            context = {'articles':recommended_list,'form':form,'form_title':form_title}
+        return render(request, 'recommender/index.html',context)
 
     else:
         form = ArticleForm()
