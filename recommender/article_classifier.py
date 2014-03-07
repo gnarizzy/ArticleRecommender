d13ac93fb10c43cb23577fedafedc7cc64a5f239
Gautam <gautam@uga.edu>
2014-03-07 00:48:25 -0500

    Finished classifier, started view to show on site

--- recommender/article_classifier.py
+++ recommender/article_classifier.py
@@ -3,23 +3,23 @@ import math
 
 #Classifies articles into one of 10 categories
 
-#returns a dictionary of unique words in text
-def get_words(text):
-    splitter=re.compile('\\W*') #non alphabet characters
-    words = [word.lower() for word in splitter.split(text)]
-    return dict([(word,1) for word in words])
-
 class classifier:
-    def __init__(self, get_features, file_name=None):
+    def __init__(self, file_name=None):
         self.feature_count={} #feature-category counts
         self.category_count={} #number of documents in each category
-        self.get_features=get_features #function used to get features, in this case get_words
+        #self.get_features=get_words #function used to get features, in this case get_words
 
     #increments feature-category count
     def inc_feature_count(self, feature, category):
         self.feature_count.setdefault(feature,{})
         self.feature_count[feature].setdefault(category,0)
         self.feature_count[feature][category]+=1
+
+    #returns a dictionary of unique words in text
+    def get_words(self,text):
+        splitter=re.compile('\\W*') #non alphabet characters
+        words = [word.lower() for word in splitter.split(text)]
+        return dict([(word,1) for word in words])
 
     #increase category count
     def inc_category_count(self, category):
@@ -48,7 +48,50 @@ class classifier:
 
     #breaks down document features, then increments counts appropriately
     def train(self,text,category):
-        features=self.get_features(text)
+        features=self.get_words(text)
         for feature in features:
             self.inc_feature_count(feature, category)
         self.inc_category_count(category)
+
+    #calculates feature probabilities
+    def feature_probability(self,feature,category):
+        if self.cat_count(category)==0:
+            return 0
+        return self.feat_count(feature,category)/self.cat_count(category)
+
+    #Begins with starting weight of .5 for reach feature
+    def weighted_probability(self, feature, category, feature_probability, weight=1.0, assumed_prob=0.5):
+        current_probability=feature_probability(feature, category)
+        total= sum([self.feat_count(feature,cat) for cat in self.categories()])
+        weighted= ((weight*assumed_prob)+(total*current_probability))/(weight+total)
+        return weighted
+
+class naive_bayes(classifier):
+    #overall probability of being a specific article, given the category
+    def article_probability(self, text, category):
+        features = self.get_words(text)
+        probability=1
+        for feature in features:
+            probability*=self.weighted_probability(feature,category,self.feature_probability)
+        return probability
+
+    #uses Bayes Theorem to give the probability of being in a category, given an article
+    def probability(self, text, category):
+        cat_prob=self.cat_count(category)/self.total_count()
+        art_prob=self.article_probability(text,category)
+        return art_prob*cat_prob
+
+    #Classifies text into category with the highest probability
+    def classify(self,text):
+        best=''
+        probabilities={}
+        max= 0.0
+        for category in self.categories():
+            probabilities[category]=self.probability(text,category)
+            if probabilities[category]>=max:
+                max=probabilities[category]
+                best=category
+        return best
+
+
+
--- recommender/views.py
+++ recommender/views.py
@@ -1,10 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from recommender.models import Article
 from recommender.forms import ArticleForm
+from recommender.article_classifier import naive_bayes
 
 def index(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST)
+        #if form.is_valid(): Add form validation
+        c1=naive_bayes()
+        article_list=Article.objects.all()
+        for article in article_list:
+            c1.train(article.content,article.category) #Should eventually be moved to database
+
+
+
     else:
         form = ArticleForm()
-    return render(request, 'recommender/index.html',{'form':form,})
+        return render(request, 'recommender/index.html',{'form':form,})
